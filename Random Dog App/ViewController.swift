d732d33371e70e80c61eb9ef5f9030d3919194ee
MaratXcode <kosjachok8@yandex.ru>
2024-07-05 19:28:03 +0300

    Initial Commit

---
+++ Random Dog App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Dog App
+//
+//  Created by Marat Fakhrizhanov on 05.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
