759023112e5b1bbf9364eade8e28df9974bb3233
MaratXcode <kosjachok8@yandex.ru>
2024-07-05 20:56:10 +0300

    work with logic

--- Random Dog App/ViewController.swift
+++ Random Dog App/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let urlString = "https://random.dog/woof.json"
+    let imageURL: String = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    
+    @IBAction func showImage() {
 
+    }
+        
+//        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+//        
+//        URLSession.shared.dataTask(with: url) { data, _, error in
+//            guard let data = data else {
+//                print(error?.localizedDescription ?? "No have data")
+//                return
+//            }
+//            
+//        }
+    
+            
+//            do {
+//                let dogJSON = try JSONDecoder().decode(DogImage.self, from: data)
+//            }
+            
+        
+    
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            guard let data = data, let response = response else {
+//                print(error?.localizedDescription ?? "No error description")
+//                return
+//            }
+//            do {
+//                let dog = try JSONDecoder().decode(DogImage.self, from: data)
+//            }
+//            
+//        } catch var error {
+//            print(error.localizedDescription)
+//        }.resume()
+        
+    
 
+    @IBAction func showSecondImage() {
+    }
+    
+    
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
 
